0bf820b71634a479329f1b2d0fc5320482ee1248
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-30 13:41:21 -0500

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -8,8 +8,6 @@ class MusicLibraryController
   end
 
   def call
-
-      #if user_input != "exit"
       puts "Welcome to your music library!"
       puts "To list all of your songs, enter 'list songs'."
       puts "To list all of the artists in your library, enter 'list artists'."
@@ -37,12 +35,54 @@ class MusicLibraryController
     end
   
     def list_artists
-      Song.all.sort! do |a,b|
-        a.artist.name <=> b.artist.name
+      Artist.all.sort! do |a,b|
+        a.name <=> b.name
       end
-      Song.all.each_with_index do |song, index|
-        puts "#{index + 1}. " + song.artist.name
+      Artist.all.each_with_index do |artist, index|
+        puts "#{index + 1}. " + artist.name
       end    
+    end
+
+    def list_genres
+      Genre.all.sort! do |a,b|
+        a.name <=> b.name
+      end
+      Genre.all.each_with_index do |genre, index|
+        puts "#{index + 1}. " + genre.name
+      end    
+    end
+
+    def list_songs_by_artist
+      puts "Please enter the name of an artist:"
+      user_input = gets.chomp
+      #if the user_input exists in the Artist class:  LOOK @ artist
+      if artist = Artist.find_by_name(user_input)
+        #note that you can daisy chain loop methods
+        artist.songs.sort! {|a,b| a.name <=> b.name }.each_with_index do |song, index|
+          puts "#{index + 1}. " + song.name + " - " + song.genre.name
+        end
+      end
+    end
+
+    def list_songs_by_genre
+      puts "Please enter the name of a genre:"
+      user_input = gets.chomp
+      if genre = Genre.find_by_name(user_input)
+        genre.songs.sort! {|a,b| a.name <=> b.name }.each_with_index do |song, index|
+          puts "#{index + 1}. " + song.artist.name + " - " + song.name
+        end
+      end
+    end
+
+    def play_song
+      puts "Which song number would you like to play?"
+      input = gets.chomp
+      index = input.to_i
+      # if index > 0 && index <= Song.all.length
+      #    new_index = index - 1
+      #    song = list_songs.find { |song| song[new_index] }
+      #   end
+      #   puts "Playing #{song.name} by #{song.artist.name}"
     end
     
 end
